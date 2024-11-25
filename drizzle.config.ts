da9288d92da0cafbc7fe8bbb3484b143f6681dbc
dnncrye <29934021+mtlaso@users.noreply.github.com>
2024-11-25 00:22:21 -0500

    db config

---
+++ drizzle.config.ts
@@ -0,0 +1,12 @@
+import "@/db/envConfig"
+import { defineConfig } from 'drizzle-kit';
+
+console.log("process :", process.env)
+export default defineConfig({
+  out: './drizzle',
+  schema: './src/db/schema.ts',
+  dialect: "postgresql",
+  dbCredentials: {
+    url: process.env.DATABASE_URL!,
+  },
+});
--- src/app/[locale]/dashboard/page.tsx
+++ src/app/[locale]/dashboard/page.tsx
@@ -1,7 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { useTranslations } from "next-intl";
+import { TbLinkPlus } from "react-icons/tb";
+import { lusitana } from "../ui/fonts";
+
 export default function Page() {
-	return (
-		<div>
-			<h1>Dashboard</h1>
-		</div>
-	);
+  const t = useTranslations("dashboard")
+  return (
+    <main className="min-h-screen max-w-2xl mx-auto px-4">
+      <section className="flex gap-2">
+        <h1 className={`${lusitana.className} font-semibold tracking-tight text-3xl`}>{t("links")}</h1>
+        <Button variant="outline" size="icon">
+          <TbLinkPlus />
+        </Button>
+      </section>
+
+      <section>
+        links...
+      </section>
+
+    </main>
+  );
 }
