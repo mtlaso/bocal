67711b864b7db0435a6d46b745477375c3a32685
dnncrye <29934021+mtlaso@users.noreply.github.com>
2024-11-28 20:59:43 -0500

    afficher urls

--- src/app/[locale]/lib/data.ts
+++ src/app/[locale]/lib/data.ts
@@ -1,12 +1,23 @@
 import { auth } from "@/auth";
+import { db } from "@/db/db";
+import { links } from "@/db/schema";
+import { eq } from "drizzle-orm";
 import "server-only";
 
-export async function getLinks(): Promise<void> {
+export async function getLinks(): Promise<{ id: number; url: string }[]> {
 	try {
 		const user = await auth();
 		if (!user) {
 			throw new Error("errors.notSignedIn");
 		}
+
+		return await db
+			.select({
+				id: links.id,
+				url: links.url,
+			})
+			.from(links)
+			.where(eq(links.userId, user.user.id));
 	} catch (_err) {
 		throw new Error("errors.unexpected");
 	}
--- src/app/[locale]/ui/dashboard/links.tsx
+++ src/app/[locale]/ui/dashboard/links.tsx
@@ -1,6 +1,14 @@
 import { getLinks } from "../../lib/data";
 
 export async function Links(): Promise<React.JSX.Element> {
-	const _links = await getLinks();
-	return <section>test</section>;
+	const links = await getLinks();
+	return (
+		<section>
+			<ul>
+				{links.map((item) => (
+					<li key={item.id}>{item.url}</li>
+				))}
+			</ul>
+		</section>
+	);
 }
