234194c9bba0d0365855fcf2bb553f7357e0175d
dnncrye <29934021+mtlaso@users.noreply.github.com>
2024-12-01 04:25:41 -0500

    menu contextuel

--- src/app/[locale]/ui/dashboard/links.tsx
+++ src/app/[locale]/ui/dashboard/links.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import {
 	Card,
 	CardContent,
@@ -5,9 +6,21 @@ import {
 	CardHeader,
 	CardTitle,
 } from "@/components/ui/card";
+import {
+	DropdownMenu,
+	DropdownMenuContent,
+	DropdownMenuGroup,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuShortcut,
+	DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { Link } from "@/i18n/routing";
+import { Trash } from "lucide-react";
 import { getTranslations } from "next-intl/server";
 import Image from "next/image";
+import { BsThreeDots } from "react-icons/bs";
 import { getLinks } from "../../lib/data";
 
 export async function Links(): Promise<React.JSX.Element> {
@@ -25,7 +38,11 @@ export async function Links(): Promise<React.JSX.Element> {
 	return (
 		<section className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-4">
 			{links.map((item) => (
-				<Card key={item.id} className="grid grid-rows-subgrid row-span-3">
+				<Card
+					key={item.id}
+					className="grid grid-rows-subgrid row-span-3 hover:shadow-md
+				dark:hover:shadow-2xl transition-all duration-200"
+				>
 					<CardHeader className="!p-0">
 						<Link href={item.url} target="_blank">
 							<Image
@@ -47,7 +64,7 @@ export async function Links(): Promise<React.JSX.Element> {
 						</CardTitle>
 					</CardContent>
 
-					<CardFooter>
+					<CardFooter className="flex justify-between">
 						<Link
 							href={item.url}
 							target="_blank"
@@ -55,9 +72,37 @@ export async function Links(): Promise<React.JSX.Element> {
 						>
 							{removeWWW(new URL(item.url).host)}
 						</Link>
+
+						<Menu delete={t("delete")} />
 					</CardFooter>
 				</Card>
 			))}
 		</section>
+	);
+}
+
+function Menu(text: {delete:string}): React.JSX.Element {
+	return (
+		<DropdownMenu>
+			<DropdownMenuTrigger asChild>
+				<Button
+					variant="link"
+					size="icon"
+					className="max-w-min text-foreground"
+				>
+					<BsThreeDots />
+				</Button>
+			</DropdownMenuTrigger>
+			<DropdownMenuContent className="w-56">
+				<DropdownMenuSeparator />
+				<DropdownMenuGroup>
+					<DropdownMenuItem>
+						<Trash />
+						<span>{text.delete}</span>
+					</DropdownMenuItem>
+				</DropdownMenuGroup>
+				<DropdownMenuSeparator />
+			</DropdownMenuContent>
+		</DropdownMenu>
 	);
 }
