63df412c8cb71d2219d24791b9bed55f0cc6b8c0
dnncrye <29934021+mtlaso@users.noreply.github.com>
2024-12-04 02:33:27 -0500

    refactor

--- src/app/[locale]/login/page.tsx
+++ src/app/[locale]/login/page.tsx
@@ -1,10 +1,10 @@
+import { lusitana } from "@/app/[locale]/ui/fonts";
 import { LoginForm } from "../ui/auth/login-form";
-import { BocalLogo } from "../ui/bocal-logo";
 
 export default function Page(): React.JSX.Element {
 	return (
 		<main className="min-h-screen flex flex-col items-center justify-center">
-			<BocalLogo className="text-4xl" />
+			<p className={`${lusitana.className} font-extrabold text-4xl`}>Bocal</p>
 			<LoginForm />
 		</main>
 	);
