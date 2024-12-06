5cec1571ea7e78949bdc9e8ce1b7a36fbf378eed
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2024-12-05 22:59:29 -0500

    trier liens par date dajout mobile+desktop

--- src/app/[locale]/ui/sort-links.tsx
+++ src/app/[locale]/ui/sort-links.tsx
@@ -1,17 +1,20 @@
 "use client";
 
+import { useSortLogic } from "@/app/[locale]/lib/hooks/use-sort-logic";
 import { sortOptions } from "@/app/[locale]/lib/schema";
+import { SPACING } from "@/app/[locale]/ui/spacing";
 import { Button } from "@/components/ui/button";
 import {
 	Drawer,
 	DrawerClose,
 	DrawerContent,
-	DrawerDescription,
 	DrawerFooter,
 	DrawerHeader,
 	DrawerTitle,
 	DrawerTrigger,
 } from "@/components/ui/drawer";
+import { Label } from "@/components/ui/label";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import {
 	Select,
 	SelectContent,
@@ -19,9 +22,9 @@ import {
 	SelectTrigger,
 	SelectValue,
 } from "@/components/ui/select";
-import { usePathname, useRouter } from "@/i18n/routing";
+import {} from "@/i18n/routing";
 import { useTranslations } from "next-intl";
-import { useSearchParams } from "next/navigation";
+import { TbArrowsSort } from "react-icons/tb";
 
 export function SortLinks(): React.JSX.Element {
 	return (
@@ -38,34 +41,12 @@ export function SortLinks(): React.JSX.Element {
 
 function SortDesktop(): React.JSX.Element {
 	const t = useTranslations("navbar");
-	const searchParams = useSearchParams();
-	const { replace } = useRouter();
-	const pathname = usePathname();
-
-	const handleChange = (value: string): void => {
-		const params = new URLSearchParams(searchParams);
-		params.set("sort", value);
-
-		replace(`${pathname}?${params.toString()}`);
-	};
-
-	const defaultValue = (): string => {
-		const sort = searchParams.get("sort")?.toString();
-
-		if (
-			sort !== undefined &&
-			(sort === sortOptions.byDateAsc || sort === sortOptions.byDateDesc)
-		) {
-			return t(`sort.${sort}`);
-		}
-
-		return t("sort.sort");
-	};
+	const { value, handleChange } = useSortLogic();
 
 	return (
-		<Select onValueChange={(e): void => handleChange(e)}>
+		<Select onValueChange={(e): void => handleChange(e)} value={value()}>
 			<SelectTrigger className="w-[180px]">
-				<SelectValue placeholder={defaultValue()} />
+				<SelectValue placeholder={t("sort.sort")} />
 			</SelectTrigger>
 			<SelectContent>
 				<SelectItem value={sortOptions.byDateAsc}>
@@ -80,21 +61,55 @@ function SortDesktop(): React.JSX.Element {
 }
 
 function SortMobile(): React.JSX.Element {
-	// Drawer
+	const t = useTranslations("navbar");
+	const { value, handleChange } = useSortLogic();
 	return (
 		<Drawer>
-			<DrawerTrigger>Open</DrawerTrigger>
+			<DrawerTrigger asChild>
+				<Button variant="outline">
+					{t("sort.sort")}
+					<TbArrowsSort />
+				</Button>
+			</DrawerTrigger>
 			<DrawerContent>
-				<DrawerHeader>
-					<DrawerTitle>Are you absolutely sure?</DrawerTitle>
-					<DrawerDescription>This action cannot be undone.</DrawerDescription>
-				</DrawerHeader>
-				<DrawerFooter>
-					<Button>Submit</Button>
-					<DrawerClose>
-						<Button variant="outline">Cancel</Button>
-					</DrawerClose>
-				</DrawerFooter>
+				<div className="mx-auto w-full max-w-sm">
+					<DrawerHeader>
+						<DrawerTitle>{t("sort.sort")}</DrawerTitle>
+					</DrawerHeader>
+
+					<div className="p-4 pb-0">
+						<RadioGroup
+							className={`${SPACING.SM}`}
+							value={value()}
+							onValueChange={(e): void => handleChange(e)}
+						>
+							<div className="flex items-center space-x-2">
+								<RadioGroupItem
+									value={sortOptions.byDateAsc}
+									id={t("sort.byDateAsc")}
+								/>
+								<Label htmlFor={t("sort.byDateAsc")}>
+									{t("sort.byDateAsc")}
+								</Label>
+							</div>
+							<div className="flex items-center space-x-2">
+								<RadioGroupItem
+									value={sortOptions.byDateDesc}
+									id={t("sort.byDateDesc")}
+								/>
+								<Label htmlFor={t("sort.byDateDesc")}>
+									{t("sort.byDateDesc")}
+								</Label>
+							</div>
+						</RadioGroup>
+					</div>
+
+					<DrawerFooter>
+						<DrawerClose asChild>
+							<Button variant="outline">{t("close")}</Button>
+						</DrawerClose>
+					</DrawerFooter>
+				</div>
 			</DrawerContent>
 		</Drawer>
 	);
