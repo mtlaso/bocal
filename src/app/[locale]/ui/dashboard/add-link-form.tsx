39361cdb769dedff9e9fe18ac5bc1f29c080e570
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2024-12-05 23:53:07 -0500

    bouton ajouter lien change de taille selon lecran

--- src/app/[locale]/ui/dashboard/add-link-form.tsx
+++ src/app/[locale]/ui/dashboard/add-link-form.tsx
@@ -1,45 +1,61 @@
 "use client";
 
+import { useMediaQuery } from "@/app/[locale]/lib/hooks/use-media-query";
 import { Button } from "@/components/ui/button";
 import {
 	Dialog,
 	DialogContent,
-	DialogFooter,
 	DialogHeader,
 	DialogTitle,
 	DialogTrigger,
 } from "@/components/ui/dialog";
+import {
+	Drawer,
+	DrawerClose,
+	DrawerContent,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
+} from "@/components/ui/drawer";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
 import { useTranslations } from "next-intl";
-import { useActionState, useEffect, useState } from "react";
+import { useActionState, useState } from "react";
 import { TbLinkPlus } from "react-icons/tb";
 import { type AddLinkState, addLink } from "../../lib/actions";
 import { lusitana } from "../fonts";
 import { SPACING } from "../spacing";
 
 export function AddLinkForm(): React.JSX.Element {
-	const initialState: AddLinkState = {
-		errors: undefined,
-		message: null,
-		data: undefined,
-	};
-	const t = useTranslations("dashboard");
-	const [state, formAction, pending] = useActionState(addLink, initialState);
 	const [isOpen, setIsOpen] = useState(false);
+	const isDesktop = useMediaQuery("(min-width: 768px)");
 
-	useEffect(() => {
-		if (
-			state.message === undefined &&
-			state.data === undefined &&
-			state.errors === undefined
-		) {
-			setIsOpen(false);
-		}
-	}, [state]);
+	return isDesktop ? (
+		<AddLinkFormDesktop
+			isOpen={isOpen}
+			onOpen={(status: boolean): void => setIsOpen(status)}
+		/>
+	) : (
+		<AddLinkFormMobile
+			isOpen={isOpen}
+			onOpen={(status: boolean): void => setIsOpen(status)}
+		/>
+	);
+}
+
+function AddLinkFormDesktop({
+	isOpen,
+	onOpen,
+}: {
+	isOpen: boolean;
+	onOpen: (status: boolean) => void;
+}): React.JSX.Element {
+	const t = useTranslations("dashboard");
 
 	return (
-		<Dialog open={isOpen} onOpenChange={(status): void => setIsOpen(status)}>
+		<Dialog open={isOpen} onOpenChange={(status): void => onOpen(status)}>
 			<DialogTrigger asChild>
 				<Button variant="outline" size="icon">
 					<TbLinkPlus />
@@ -51,41 +67,89 @@ export function AddLinkForm(): React.JSX.Element {
 						{t("addLinkForm.title")}
 					</DialogTitle>
 				</DialogHeader>
-				<form action={formAction} id="form">
-					<div className={`${SPACING.SM}`}>
-						<Label htmlFor="url" className="block text-sm font-medium">
-							{t("addLinkForm.link")}
-						</Label>
-
-						<div className="relative">
-							<Input
-								className="block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-								name="url"
-								autoFocus
-								id="url"
-								placeholder="https://..."
-								defaultValue={state.data?.url}
-							/>
-
-							<TbLinkPlus className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
-						</div>
-
-						{state.errors?.url?.map((err) => (
-							<p className="mt-2 text-sm text-red-500" key={err}>
-								{t(err)}
-							</p>
-						))}
-						{state?.message && (
-							<p className="mt-2 text-sm text-red-500">{t(state.message)}</p>
-						)}
-					</div>
-				</form>
-				<DialogFooter>
-					<Button disabled={pending} type="submit" form="form">
-						{t("add")}
-					</Button>
-				</DialogFooter>
+				<LinkForm />
 			</DialogContent>
 		</Dialog>
+	);
+}
+
+function AddLinkFormMobile({
+	isOpen,
+	onOpen,
+}: {
+	isOpen: boolean;
+	onOpen: (status: boolean) => void;
+}): React.JSX.Element {
+	const t = useTranslations("dashboard");
+	return (
+		<Drawer open={isOpen} onOpenChange={onOpen}>
+			<DrawerTrigger asChild>
+				<Button variant="outline" size="icon">
+					<TbLinkPlus />
+				</Button>
+			</DrawerTrigger>
+			<DrawerContent>
+				<DrawerHeader className={cn("text-left", lusitana.className)}>
+					<DrawerTitle>{t("addLinkForm.title")}</DrawerTitle>
+				</DrawerHeader>
+				<LinkForm className="px-4" />
+				<DrawerFooter className="pt-2">
+					<DrawerClose asChild>
+						<Button variant="outline">{t("addLinkForm.cancel")}</Button>
+					</DrawerClose>
+				</DrawerFooter>
+			</DrawerContent>
+		</Drawer>
+	);
+}
+
+function LinkForm({
+	className,
+}: React.ComponentProps<"form">): React.JSX.Element {
+	const initialState: AddLinkState = {
+		errors: undefined,
+		message: null,
+		data: undefined,
+	};
+	const t = useTranslations("dashboard");
+	const [state, formAction, pending] = useActionState(addLink, initialState);
+	return (
+		<form
+			className={cn(SPACING.MD, "grid", className)}
+			action={formAction}
+			id="form"
+		>
+			<div className={`${SPACING.SM}`}>
+				<Label htmlFor="url" className="block text-sm font-medium">
+					{t("addLinkForm.link")}
+				</Label>
+
+				<div className="relative">
+					<Input
+						className="block w-full cursor-pointer rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
+						name="url"
+						autoFocus
+						id="url"
+						placeholder="https://..."
+						defaultValue={state.data?.url}
+					/>
+
+					<TbLinkPlus className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+				</div>
+
+				{state.errors?.url?.map((err) => (
+					<p className="mt-2 text-sm text-red-500" key={err}>
+						{t(err)}
+					</p>
+				))}
+				{state?.message && (
+					<p className="mt-2 text-sm text-red-500">{t(state.message)}</p>
+				)}
+			</div>
+
+			<Button disabled={pending} type="submit" form="form">
+				{t("add")}
+			</Button>
+		</form>
 	);
 }
