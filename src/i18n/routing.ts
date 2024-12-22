7d9c1d6c39e542c7bcf88df9c2bfba42bee50810
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2024-12-22 05:09:11 -0500

    cacher les prefix de langues pour que oauth fonctionne

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -4,12 +4,12 @@ import { defineRouting } from "next-intl/routing";
 export type Locale = (typeof routing.locales)[number];
 
 export const routing = defineRouting({
-	locales: ["en", "fr"],
-	defaultLocale: "en",
-	localePrefix: "as-needed",
+  locales: ["en", "fr"],
+  defaultLocale: "en",
+  localePrefix: "never",
 });
 
 export type Locales = (typeof routing.locales)[number];
 
 export const { Link, redirect, usePathname, useRouter, getPathname } =
-	createNavigation(routing);
+  createNavigation(routing);
