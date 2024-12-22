3baa7c725ccef26695ea148dd6b182e6a07513de
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2024-12-22 04:05:38 -0500

    ajout sitemap

---
+++ src/app/sitemap.tsx
@@ -0,0 +1,39 @@
+import { type Locale, getPathname, routing } from "@/i18n/routing";
+import type { MetadataRoute } from "next";
+
+const HOST = process.env.APP_URL;
+
+export default function sitemap(): MetadataRoute.Sitemap {
+	return [
+		getEntry("/"),
+		getEntry("/dashboard"),
+		getEntry("/archive"),
+		getEntry("/feed"),
+		getEntry("/login"),
+	];
+}
+
+type Href = Parameters<typeof getPathname>[0]["href"];
+
+function getEntry(href: Href): {
+	url: string;
+	alternates: {
+		languages: {
+			[k: string]: string;
+		};
+	};
+} {
+	return {
+		url: getUrl(href, routing.defaultLocale),
+		alternates: {
+			languages: Object.fromEntries(
+				routing.locales.map((locale) => [locale, getUrl(href, locale)]),
+			),
+		},
+	};
+}
+
+function getUrl(href: Href, locale: Locale): string {
+	const pathname = getPathname({ locale, href });
+	return HOST + pathname;
+}
--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -1,6 +1,8 @@
 import { createNavigation } from "next-intl/navigation";
 import { defineRouting } from "next-intl/routing";
 
+export type Locale = (typeof routing.locales)[number];
+
 export const routing = defineRouting({
 	locales: ["en", "fr"],
 	defaultLocale: "en",
