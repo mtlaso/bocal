afde2fe2fa5f15d6522382ff66d78afd4e990145
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2024-12-24 00:50:21 -0500

    branch test

--- src/app/[locale]/(app)/dashboard/page.tsx
+++ src/app/[locale]/(app)/dashboard/page.tsx
@@ -9,46 +9,45 @@ import { Links } from "../../ui/links";
 import { LinksSkeleton } from "../../ui/skeletons";
 
 export async function generateMetadata({
-	params,
+  params,
 }: {
-	params: Promise<{ locale: string }>;
+  params: Promise<{ locale: string }>;
 }): Promise<Metadata> {
-	const { locale } = await params;
-	const t = await getTranslations({ locale, namespace: "metadata.dashboard" });
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: "metadata.dashboard" });
 
-	return {
-		title: t("title"),
-		description: t("description"),
-	} satisfies Metadata;
+  return {
+    title: t("title"),
+    description: t("description"),
+  } satisfies Metadata;
 }
 
 export default async function Page({
-	searchParams,
+  searchParams,
 }: {
-	// searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
-	searchParams: Promise<{ sort?: string }>;
+  searchParams: Promise<{ sort?: string }>;
 }): Promise<React.JSX.Element> {
-	const { sort } = await searchParams;
-	const t = await getTranslations("dashboard");
-	const links = await getLinks({ sort });
+  const { sort } = await searchParams;
+  const t = await getTranslations("dashboard");
+  const links = await getLinks({ sort });
 
-	return (
-		<>
-			<section className="flex justify-between">
-				<div className="flex gap-2">
-					<h1 className="font-semibold tracking-tight text-3xl">
-						{t("links")}
-					</h1>
-					<AddLinkForm />
-				</div>
-				<SortLinks />
-			</section>
+  return (
+    <>
+      <section className="flex justify-between">
+        <div className="flex gap-2">
+          <h1 className="font-semibold tracking-tight text-3xl">
+            {t("links")}
+          </h1>
+          <AddLinkForm />
+        </div>
+        <SortLinks />
+      </section>
 
-			<Separator className="my-4" />
+      <Separator className="my-4" />
 
-			<Suspense fallback={<LinksSkeleton />}>
-				<Links links={links} view={"grid"} />
-			</Suspense>
-		</>
-	);
+      <Suspense fallback={<LinksSkeleton />}>
+        <Links links={links} view={"grid"} />
+      </Suspense>
+    </>
+  );
 }
