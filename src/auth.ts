a4b70eb39f77cb4745bcc83d51edb014e4eaab73
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2024-12-26 20:44:17 -0500

    test

--- src/auth.ts
+++ src/auth.ts
@@ -4,50 +4,49 @@ import GitHub from "next-auth/providers/github";
 import Google from "next-auth/providers/google";
 import { db } from "./db/db";
 import {
-	accounts,
-	authenticators,
-	sessions,
-	users,
-	verificationTokens,
+  accounts,
+  authenticators,
+  sessions,
+  users,
+  verificationTokens,
 } from "./db/schema";
 
 declare module "next-auth" {
-	/**
-	 * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
-	 */
-	interface Session {
-		user: {
-			id: string;
-		} & DefaultSession["user"];
-	}
+  /**
+   * Returned by `auth`, `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
+   */
+  interface Session {
+    user: {
+      id: string;
+    } & DefaultSession["user"];
+  }
 }
 
 const config = {
-	providers: [GitHub, Google],
-	pages: {
-		signIn: "/login",
-		newUser: "/dashboard",
-	},
-	adapter: DrizzleAdapter(db, {
-		usersTable: users,
-		accountsTable: accounts,
-		sessionsTable: sessions,
-		verificationTokensTable: verificationTokens,
-		authenticatorsTable: authenticators,
-	}),
-
-	callbacks: {
-		// biome-ignore lint/nursery/useExplicitType: auth.ts
-		session({ session }) {
-			return {
-				...session,
-				user: {
-					...session.user,
-					id: session.user.id,
-				},
-			};
-		},
-	},
+  providers: [GitHub, Google],
+  pages: {
+    signIn: "/login",
+    newUser: "/dashboard",
+  },
+  adapter: DrizzleAdapter(db, {
+    usersTable: users,
+    accountsTable: accounts,
+    sessionsTable: sessions,
+    verificationTokensTable: verificationTokens,
+    authenticatorsTable: authenticators,
+  }),
+  callbacks: {
+    // biome-ignore lint/nursery/useExplicitType: auth.ts
+    session({ session }) {
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          id: session.user.id,
+        },
+      };
+    },
+  },
 } satisfies NextAuthConfig;
 
 export const { handlers, signIn, signOut, auth } = NextAuth(config);
