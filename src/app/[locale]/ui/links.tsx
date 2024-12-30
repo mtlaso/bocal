fd20d9c7a4a86e3dec9f2367df76b6e5da157a75
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2024-12-29 21:35:17 -0500

    fix: ajout condition par defaut

--- src/app/[locale]/ui/links.tsx
+++ src/app/[locale]/ui/links.tsx
@@ -4,11 +4,11 @@ import { removeWWW } from "@/app/[locale]/lib/remove-www";
 import { searchParamsParsers } from "@/app/[locale]/lib/stores/search-params";
 import { SortOptions } from "@/app/[locale]/lib/types";
 import {
-	Card,
-	CardContent,
-	CardFooter,
-	CardHeader,
-	CardTitle,
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
 } from "@/components/ui/card";
 import { Link } from "@/i18n/routing";
 import { cn } from "@/lib/utils";
@@ -18,131 +18,133 @@ import { useQueryStates } from "nuqs";
 import { LinksContextMenu } from "./links-context-menu";
 
 type Props = {
-	links: {
-		id: number;
-		url: string;
-		ogTitle: string | null;
-		ogImageURL: string | null;
-	}[];
-	view: "grid" | "list";
+  links: {
+    id: number;
+    url: string;
+    ogTitle: string | null;
+    ogImageURL: string | null;
+  }[];
+  view: "grid" | "list";
 };
 
 export function Links({ links, view }: Props): React.JSX.Element {
-	const [{ sortLinks }] = useQueryStates(searchParamsParsers);
-	const t = useTranslations("dashboard");
+  const [{ sortLinks }] = useQueryStates(searchParamsParsers);
+  const t = useTranslations("dashboard");
 
-	const items = [...links];
-	switch (sortLinks) {
-		case SortOptions.BY_DATE_ASC:
-			items.sort((a, b) => a.id - b.id);
-			break;
-		case SortOptions.BY_DATE_DESC:
-			items.sort((a, b) => b.id - a.id);
-			break;
-	}
+  const items = [...links];
+  switch (sortLinks) {
+    case SortOptions.BY_DATE_ASC:
+      items.sort((a, b) => a.id - b.id);
+      break;
+    case SortOptions.BY_DATE_DESC:
+      items.sort((a, b) => b.id - a.id);
+      break;
+    default:
+      break;
+  }
 
-	const randomBackground = (firstLetter: string): string => {
-		const letter = firstLetter.toUpperCase();
-		const charCode = letter.charCodeAt(0);
+  const randomBackground = (firstLetter: string): string => {
+    const letter = firstLetter.toUpperCase();
+    const charCode = letter.charCodeAt(0);
 
-		if (charCode >= 65 && charCode <= 68) {
-			return "from-[#FFB6B9] to-[#FF79C6]";
-		}
+    if (charCode >= 65 && charCode <= 68) {
+      return "from-[#FFB6B9] to-[#FF79C6]";
+    }
 
-		if (charCode >= 69 && charCode <= 72) {
-			return "from-red-200 to-indigo-600";
-		}
+    if (charCode >= 69 && charCode <= 72) {
+      return "from-red-200 to-indigo-600";
+    }
 
-		return "from-emerald-300 to-violet-700";
-	};
+    return "from-emerald-300 to-violet-700";
+  };
 
-	return (
-		<section
-			className={cn({
-				"grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4": view === "grid",
-				"grid grid-cols-3 grid-rows-2 gap-4": view === "list",
-			})}
-		>
-			{items.map((item) => (
-				<Card
-					key={item.id}
-					className={cn(
-						{
-							"grid grid-rows-subgrid row-span-3 col-span-1": view === "grid",
-							"grid grid-cols-subgrid col-span-3 row-span-2 items-center":
-								view === "list",
-						},
-						"hover:shadow-md dark:hover:shadow-xl transition-all duration-200 break-all",
-					)}
-				>
-					<CardHeader
-						className={cn("!p-0", {
-							"row-start- row-span-2": view === "list",
-						})}
-					>
-						<Link href={item.url} target="_blank">
-							{item.ogImageURL ? (
-								<Image
-									className={cn("aspect-video h-auto w-full object-cover", {
-										"rounded-t-xl": view === "grid",
-										"rounded-bl-xl rounded-tl-xl": view === "list",
-									})}
-									src={item.ogImageURL}
-									width={500}
-									height={500}
-									priority={false}
-									alt={t("ogImageAlt")}
-								/>
-							) : (
-								<div
-									className={cn(
-										`aspect-video h-auto w-full object-cover
+  return (
+    <section
+      className={cn({
+        "grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4": view === "grid",
+        "grid grid-cols-3 grid-rows-2 gap-4": view === "list",
+      })}
+    >
+      {items.map((item) => (
+        <Card
+          key={item.id}
+          className={cn(
+            {
+              "grid grid-rows-subgrid row-span-3 col-span-1": view === "grid",
+              "grid grid-cols-subgrid col-span-3 row-span-2 items-center":
+                view === "list",
+            },
+            "hover:shadow-md dark:hover:shadow-xl transition-all duration-200 break-all",
+          )}
+        >
+          <CardHeader
+            className={cn("!p-0", {
+              "row-start- row-span-2": view === "list",
+            })}
+          >
+            <Link href={item.url} target="_blank">
+              {item.ogImageURL ? (
+                <Image
+                  className={cn("aspect-video h-auto w-full object-cover", {
+                    "rounded-t-xl": view === "grid",
+                    "rounded-bl-xl rounded-tl-xl": view === "list",
+                  })}
+                  src={item.ogImageURL}
+                  width={500}
+                  height={500}
+                  priority={false}
+                  alt={t("ogImageAlt")}
+                />
+              ) : (
+                <div
+                  className={cn(
+                    `aspect-video h-auto w-full object-cover
 										select-none text-9xl text-foreground font-semibold
 										bg-gradient-to-br`,
-										randomBackground(
-											item.ogTitle ?? removeWWW(new URL(item.url).host),
-										),
-										{
-											"rounded-t-xl": view === "grid",
-											"rounded-bl-xl rounded-tl-xl": view === "list",
-										},
-									)}
-								/>
-							)}
-						</Link>
-					</CardHeader>
+                    randomBackground(
+                      item.ogTitle ?? removeWWW(new URL(item.url).host),
+                    ),
+                    {
+                      "rounded-t-xl": view === "grid",
+                      "rounded-bl-xl rounded-tl-xl": view === "list",
+                    },
+                  )}
+                />
+              )}
+            </Link>
+          </CardHeader>
 
-					<CardContent
-						className={cn({
-							"row-start-1 col-start-2 col-span-2 p-0 ": view === "list",
-						})}
-					>
-						<CardTitle>
-							<Link href={item.url} target="_blank" className="line-clamp-3">
-								{item.ogTitle ?? new URL(item.url).host}
-							</Link>
-						</CardTitle>
-					</CardContent>
+          <CardContent
+            className={cn({
+              "row-start-1 col-start-2 col-span-2 p-0 ": view === "list",
+            })}
+          >
+            <CardTitle>
+              <Link href={item.url} target="_blank" className="line-clamp-3">
+                {item.ogTitle ?? new URL(item.url).host}
+              </Link>
+            </CardTitle>
+          </CardContent>
 
-					<CardFooter
-						className={cn("grid grid-cols2 grid-cols-[90%_1fr] gap-2", {
-							"row-start-2 col-start-2 col-span-2 pl-0": view === "list",
-						})}
-					>
-						<Link
-							href={item.url}
-							target="_blank"
-							className=" text-sm text-muted-foreground truncate"
-						>
-							{removeWWW(new URL(item.url).host)}
-						</Link>
+          <CardFooter
+            className={cn("grid grid-cols2 grid-cols-[90%_1fr] gap-2", {
+              "row-start-2 col-start-2 col-span-2 pl-0": view === "list",
+            })}
+          >
+            <Link
+              href={item.url}
+              target="_blank"
+              className=" text-sm text-muted-foreground truncate"
+            >
+              {removeWWW(new URL(item.url).host)}
+            </Link>
 
-						<div>
-							<LinksContextMenu id={item.id.toString()} />
-						</div>
-					</CardFooter>
-				</Card>
-			))}
-		</section>
-	);
+            <div>
+              <LinksContextMenu id={item.id.toString()} />
+            </div>
+          </CardFooter>
+        </Card>
+      ))}
+    </section>
+  );
 }
--- src/app/[locale]/ui/sort-links.tsx
+++ src/app/[locale]/ui/sort-links.tsx
@@ -4,120 +4,120 @@ import { SortOptions } from "@/app/[locale]/lib/types";
 import { SPACING } from "@/app/[locale]/ui/spacing";
 import { Button } from "@/components/ui/button";
 import {
-	Drawer,
-	DrawerClose,
-	DrawerContent,
-	DrawerFooter,
-	DrawerHeader,
-	DrawerTitle,
-	DrawerTrigger,
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
 } from "@/components/ui/drawer";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
 import {
-	Select,
-	SelectContent,
-	SelectItem,
-	SelectTrigger,
-	SelectValue,
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
 } from "@/components/ui/select";
 import { useTranslations } from "next-intl";
 import { useQueryStates } from "nuqs";
 import { TbArrowsSort } from "react-icons/tb";
 
 export function SortLinks(): React.JSX.Element {
-	return (
-		<>
-			<div className="md:hidden">
-				<SortMobile />
-			</div>
-			<div className="hidden md:block">
-				<SortDesktop />
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="md:hidden">
+        <SortMobile />
+      </div>
+      <div className="hidden md:block">
+        <SortDesktop />
+      </div>
+    </>
+  );
 }
 
 function SortDesktop(): React.JSX.Element {
-	const t = useTranslations("navbar");
-	const [{ sortLinks }, setSortLinks] = useQueryStates(searchParamsParsers);
+  const t = useTranslations("navbar");
+  const [{ sortLinks }, setSortLinks] = useQueryStates(searchParamsParsers);
 
-	return (
-		<Select
-			onValueChange={(e): void => {
-				setSortLinks({ sortLinks: e as SortOptions });
-			}}
-			value={sortLinks}
-		>
-			<SelectTrigger className="w-[180px]">
-				<SelectValue placeholder={t("sort.sort")} />
-			</SelectTrigger>
-			<SelectContent>
-				<SelectItem value={SortOptions.BY_DATE_ASC}>
-					{t("sort.byDateAsc")}
-				</SelectItem>
-				<SelectItem value={SortOptions.BY_DATE_DESC}>
-					{t("sort.byDateDesc")}
-				</SelectItem>
-			</SelectContent>
-		</Select>
-	);
+  return (
+    <Select
+      onValueChange={(e): void => {
+        setSortLinks({ sortLinks: e as SortOptions });
+      }}
+      value={sortLinks}
+    >
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder={t("sort.sort")} />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value={SortOptions.BY_DATE_ASC}>
+          {t("sort.byDateAsc")}
+        </SelectItem>
+        <SelectItem value={SortOptions.BY_DATE_DESC}>
+          {t("sort.byDateDesc")}
+        </SelectItem>
+      </SelectContent>
+    </Select>
+  );
 }
 
 function SortMobile(): React.JSX.Element {
-	const t = useTranslations("navbar");
-	const [{ sortLinks }, setSortLinks] = useQueryStates(searchParamsParsers);
+  const t = useTranslations("navbar");
+  const [{ sortLinks }, setSortLinks] = useQueryStates(searchParamsParsers);
 
-	return (
-		<Drawer>
-			<DrawerTrigger asChild>
-				<Button variant="outline">
-					{t("sort.sort")}
-					<TbArrowsSort />
-				</Button>
-			</DrawerTrigger>
-			<DrawerContent>
-				<div className="mx-auto w-full max-w-sm">
-					<DrawerHeader>
-						<DrawerTitle>{t("sort.sort")}</DrawerTitle>
-					</DrawerHeader>
+  return (
+    <Drawer>
+      <DrawerTrigger asChild>
+        <Button variant="outline">
+          {t("sort.sort")}
+          <TbArrowsSort />
+        </Button>
+      </DrawerTrigger>
+      <DrawerContent>
+        <div className="mx-auto w-full max-w-sm">
+          <DrawerHeader>
+            <DrawerTitle>{t("sort.sort")}</DrawerTitle>
+          </DrawerHeader>
 
-					<div className="p-4 pb-0">
-						<RadioGroup
-							className={`${SPACING.SM}`}
-							value={sortLinks}
-							onValueChange={(e): void => {
-								setSortLinks({ sortLinks: e as SortOptions });
-							}}
-						>
-							<div className="flex items-center space-x-2">
-								<RadioGroupItem
-									value={SortOptions.BY_DATE_ASC}
-									id={t("sort.byDateAsc")}
-								/>
-								<Label htmlFor={t("sort.byDateAsc")}>
-									{t("sort.byDateAsc")}
-								</Label>
-							</div>
-							<div className="flex items-center space-x-2">
-								<RadioGroupItem
-									value={SortOptions.BY_DATE_DESC}
-									id={t("sort.byDateDesc")}
-								/>
-								<Label htmlFor={t("sort.byDateDesc")}>
-									{t("sort.byDateDesc")}
-								</Label>
-							</div>
-						</RadioGroup>
-					</div>
+          <div className="p-4 pb-0">
+            <RadioGroup
+              className={`${SPACING.SM}`}
+              value={sortLinks}
+              onValueChange={(e): void => {
+                setSortLinks({ sortLinks: e as SortOptions });
+              }}
+            >
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem
+                  value={SortOptions.BY_DATE_ASC}
+                  id={t("sort.byDateAsc")}
+                />
+                <Label htmlFor={t("sort.byDateAsc")}>
+                  {t("sort.byDateAsc")}
+                </Label>
+              </div>
+              <div className="flex items-center space-x-2">
+                <RadioGroupItem
+                  value={SortOptions.BY_DATE_DESC}
+                  id={t("sort.byDateDesc")}
+                />
+                <Label htmlFor={t("sort.byDateDesc")}>
+                  {t("sort.byDateDesc")}
+                </Label>
+              </div>
+            </RadioGroup>
+          </div>
 
-					<DrawerFooter>
-						<DrawerClose asChild>
-							<Button variant="outline">{t("close")}</Button>
-						</DrawerClose>
-					</DrawerFooter>
-				</div>
-			</DrawerContent>
-		</Drawer>
-	);
+          <DrawerFooter>
+            <DrawerClose asChild>
+              <Button variant="outline">{t("close")}</Button>
+            </DrawerClose>
+          </DrawerFooter>
+        </div>
+      </DrawerContent>
+    </Drawer>
+  );
 }
