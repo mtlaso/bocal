4712e13b72dc15488e043925a09a49462bbb756d
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2025-01-02 21:27:51 -0500

    menu navigation

---
+++ src/app/[locale]/(app)/settings/page.tsx
@@ -0,0 +1,36 @@
+import { Separator } from "@/components/ui/separator";
+import { Metadata } from "next";
+import { getTranslations } from "next-intl/server";
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}): Promise<Metadata> {
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: "metadata.settings" });
+
+  return {
+    title: t("title"),
+    description: t("description"),
+  } satisfies Metadata;
+}
+
+export default async function Page(): Promise<React.JSX.Element> {
+  const t = await getTranslations("settings");
+  return (
+    <>
+      <section className="flex justify-between">
+        <h1 className="font-semibold tracking-tight text-3xl">
+          {t("title")}
+        </h1>
+      </section>
+
+      <Separator className="my-4" />
+
+      <section>
+        ...
+      </section>
+    </>
+  );
+}
