575be8deb49bf1e6fec1bfee685488e45f9ba39f
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2025-01-02 22:58:25 -0500

    feat: page paramètres

--- src/app/[locale]/(app)/settings/page.tsx
+++ src/app/[locale]/(app)/settings/page.tsx
@@ -1,36 +1,36 @@
+import { Settings } from "@/app/[locale]/ui/settings/settings";
+import { auth } from "@/auth";
 import { Separator } from "@/components/ui/separator";
-import { Metadata } from "next";
+import type { Metadata } from "next";
 import { getTranslations } from "next-intl/server";
 
 export async function generateMetadata({
-  params,
+	params,
 }: {
-  params: Promise<{ locale: string }>;
+	params: Promise<{ locale: string }>;
 }): Promise<Metadata> {
-  const { locale } = await params;
-  const t = await getTranslations({ locale, namespace: "metadata.settings" });
+	const { locale } = await params;
+	const t = await getTranslations({ locale, namespace: "metadata.settings" });
 
-  return {
-    title: t("title"),
-    description: t("description"),
-  } satisfies Metadata;
+	return {
+		title: t("title"),
+		description: t("description"),
+	} satisfies Metadata;
 }
 
 export default async function Page(): Promise<React.JSX.Element> {
-  const t = await getTranslations("settings");
-  return (
-    <>
-      <section className="flex justify-between">
-        <h1 className="font-semibold tracking-tight text-3xl">
-          {t("title")}
-        </h1>
-      </section>
+	const t = await getTranslations("settings");
+	const user = await auth();
 
-      <Separator className="my-4" />
+	return (
+		<>
+			<section className="flex justify-between">
+				<h1 className="font-semibold tracking-tight text-3xl">{t("title")}</h1>
+			</section>
 
-      <section>
-        ...
-      </section>
-    </>
-  );
+			<Separator className="my-4" />
+
+			<Settings email={user?.user.email ?? ""} name={user?.user.name ?? ""} />
+		</>
+	);
 }
--- src/app/[locale]/ui/navigation-menu.tsx
+++ src/app/[locale]/ui/navigation-menu.tsx
@@ -2,19 +2,19 @@
 import { Button } from "@/components/ui/button";
 import { LocaleToggle } from "@/components/ui/locale-toggle";
 import {
-  NavigationMenu,
-  NavigationMenuItem,
-  NavigationMenuLink,
-  NavigationMenuList,
-  navigationMenuTriggerStyle,
+	NavigationMenu,
+	NavigationMenuItem,
+	NavigationMenuLink,
+	NavigationMenuList,
+	navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import {
-  Sheet,
-  SheetContent,
-  SheetFooter,
-  SheetHeader,
-  SheetTitle,
-  SheetTrigger,
+	Sheet,
+	SheetContent,
+	SheetFooter,
+	SheetHeader,
+	SheetTitle,
+	SheetTrigger,
 } from "@/components/ui/sheet";
 import { ThemeToggle } from "@/components/ui/theme-toggle";
 import { Link, usePathname } from "@/i18n/routing";
@@ -25,181 +25,165 @@ import { TbMenu2 } from "react-icons/tb";
 import { LogoutForm } from "./auth/logout-form";
 
 export function NavMenu(): React.JSX.Element {
-  return (
-    <>
-      <div className="md:hidden">
-        <MobileNavMenu />
-      </div>
-      <div className="hidden md:block">
-        <DesktopNavMenu />
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="md:hidden">
+				<MobileNavMenu />
+			</div>
+			<div className="hidden md:block">
+				<DesktopNavMenu />
+			</div>
+		</>
+	);
 }
 
 function DesktopNavMenu(): React.JSX.Element {
-  const t = useTranslations("navbar");
-  const pathname = usePathname();
+	const t = useTranslations("navbar");
+	const pathname = usePathname();
 
-  return (
-    <NavigationMenu className="py-5 mb-6 !max-w-full justify-between">
-      <NavigationMenuList>
-        <NavigationMenuItem>
-          <Link
-            href="/dashboard"
-            legacyBehavior
-            passHref
-          >
-            <NavigationMenuLink
-              className={cn(navigationMenuTriggerStyle(), {
-                "font-semibold bg-primary": pathname === "/dashboard",
-              })}
-            >
-              {t("links")}
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
+	return (
+		<NavigationMenu className="py-5 mb-6 !max-w-full justify-between">
+			<NavigationMenuList>
+				<NavigationMenuItem>
+					<Link href="/dashboard" legacyBehavior passHref>
+						<NavigationMenuLink
+							className={cn(navigationMenuTriggerStyle(), {
+								"font-semibold bg-primary": pathname === "/dashboard",
+							})}
+						>
+							{t("links")}
+						</NavigationMenuLink>
+					</Link>
+				</NavigationMenuItem>
 
-        <NavigationMenuItem>
-          <Link
-            href="/archive"
-            legacyBehavior
-            passHref
-          >
-            <NavigationMenuLink
-              className={cn(navigationMenuTriggerStyle(), {
-                "font-semibold bg-primary": pathname === "/archive",
-              })}
-            >
-              {t("archive")}
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
+				<NavigationMenuItem>
+					<Link href="/archive" legacyBehavior passHref>
+						<NavigationMenuLink
+							className={cn(navigationMenuTriggerStyle(), {
+								"font-semibold bg-primary": pathname === "/archive",
+							})}
+						>
+							{t("archive")}
+						</NavigationMenuLink>
+					</Link>
+				</NavigationMenuItem>
 
-        <NavigationMenuItem>
-          <Link
-            href={"/feed"}
-            legacyBehavior
-            passHref
-          >
-            <NavigationMenuLink
-              className={cn(navigationMenuTriggerStyle(), {
-                "font-semibold bg-primary": pathname === "/feed",
-              })}
-            >
-              {t("rssFeed")}
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
+				<NavigationMenuItem>
+					<Link href={"/feed"} legacyBehavior passHref>
+						<NavigationMenuLink
+							className={cn(navigationMenuTriggerStyle(), {
+								"font-semibold bg-primary": pathname === "/feed",
+							})}
+						>
+							{t("rssFeed")}
+						</NavigationMenuLink>
+					</Link>
+				</NavigationMenuItem>
 
-        <NavigationMenuItem>
-          <Link
-            href="/settings"
-            legacyBehavior
-            passHref
-          >
-            <NavigationMenuLink
-              className={cn(navigationMenuTriggerStyle(), {
-                "font-semibold bg-primary": pathname === "/settings",
-              })}
-            >
-              {t("settings")}
-            </NavigationMenuLink>
-          </Link>
-        </NavigationMenuItem>
-      </NavigationMenuList>
+				<NavigationMenuItem>
+					<Link href="/settings" legacyBehavior passHref>
+						<NavigationMenuLink
+							className={cn(navigationMenuTriggerStyle(), {
+								"font-semibold bg-primary": pathname === "/settings",
+							})}
+						>
+							{t("settings")}
+						</NavigationMenuLink>
+					</Link>
+				</NavigationMenuItem>
+			</NavigationMenuList>
 
-      <span className="flex flex-wrap gap-2">
-        <ThemeToggle />
-        <LocaleToggle />
-        <LogoutForm />
-      </span>
-    </NavigationMenu>
-  );
+			<span className="flex flex-wrap gap-2">
+				<ThemeToggle />
+				<LocaleToggle />
+				<LogoutForm />
+			</span>
+		</NavigationMenu>
+	);
 }
 
 function MobileNavMenu(): React.JSX.Element {
-  const t = useTranslations("navbar");
-  const pathname = usePathname();
-  const [isOpen, setIsOpen] = useState(false);
+	const t = useTranslations("navbar");
+	const pathname = usePathname();
+	const [isOpen, setIsOpen] = useState(false);
 
-  return (
-    <Sheet open={isOpen} onOpenChange={(status): void => setIsOpen(status)}>
-      <SheetTrigger asChild>
-        <Button className="py-5 mb-6" variant="outline" size="icon">
-          <TbMenu2 />
-        </Button>
-      </SheetTrigger>
-      <SheetContent className="w-full">
-        <SheetHeader>
-          <SheetTitle className="mb-6 text-left text-4xl leading-tight">
-            {t("menu")}
-          </SheetTitle>
-        </SheetHeader>
-        <div className="flex flex-col gap-4">
-          <Link
-            onClick={(): void => setIsOpen(false)}
-            href={"/dashboard"}
-            className={cn(
-              navigationMenuTriggerStyle(),
-              "w-full flex justify-start ",
-              {
-                "font-semibold bg-primary": pathname === "/dashboard",
-              },
-            )}
-          >
-            <span>{t("links")}</span>
-          </Link>
+	return (
+		<Sheet open={isOpen} onOpenChange={(status): void => setIsOpen(status)}>
+			<SheetTrigger asChild>
+				<Button className="py-5 mb-6" variant="outline" size="icon">
+					<TbMenu2 />
+				</Button>
+			</SheetTrigger>
+			<SheetContent className="w-full">
+				<SheetHeader>
+					<SheetTitle className="mb-6 text-left text-4xl leading-tight">
+						{t("menu")}
+					</SheetTitle>
+				</SheetHeader>
+				<div className="flex flex-col gap-4">
+					<Link
+						onClick={(): void => setIsOpen(false)}
+						href={"/dashboard"}
+						className={cn(
+							navigationMenuTriggerStyle(),
+							"w-full flex justify-start ",
+							{
+								"font-semibold bg-primary": pathname === "/dashboard",
+							},
+						)}
+					>
+						<span>{t("links")}</span>
+					</Link>
 
-          <Link
-            onClick={(): void => setIsOpen(false)}
-            href={"/archive"}
-            className={cn(
-              navigationMenuTriggerStyle(),
-              "w-full flex justify-start ",
-              {
-                "font-semibold bg-primary": pathname === "/archive",
-              },
-            )}
-          >
-            <span className="">{t("archive")}</span>
-          </Link>
+					<Link
+						onClick={(): void => setIsOpen(false)}
+						href={"/archive"}
+						className={cn(
+							navigationMenuTriggerStyle(),
+							"w-full flex justify-start ",
+							{
+								"font-semibold bg-primary": pathname === "/archive",
+							},
+						)}
+					>
+						<span className="">{t("archive")}</span>
+					</Link>
 
-          <Link
-            onClick={(): void => setIsOpen(false)}
-            href={"/feed"}
-            className={cn(
-              navigationMenuTriggerStyle(),
-              "w-full flex justify-start ",
-              {
-                "font-semibold bg-primary": pathname === "/feed",
-              },
-            )}
-          >
-            <span className="">{t("rssFeed")}</span>
-          </Link>
+					<Link
+						onClick={(): void => setIsOpen(false)}
+						href={"/feed"}
+						className={cn(
+							navigationMenuTriggerStyle(),
+							"w-full flex justify-start ",
+							{
+								"font-semibold bg-primary": pathname === "/feed",
+							},
+						)}
+					>
+						<span className="">{t("rssFeed")}</span>
+					</Link>
 
-          <Link
-            onClick={(): void => setIsOpen(false)}
-            href={"/settings"}
-            className={cn(
-              navigationMenuTriggerStyle(),
-              "w-full flex justify-start ",
-              {
-                "font-semibold bg-primary": pathname === "/settings",
-              },
-            )}
-          >
-            <span className="">{t("rssFeed")}</span>
-          </Link>
-        </div>
+					<Link
+						onClick={(): void => setIsOpen(false)}
+						href={"/settings"}
+						className={cn(
+							navigationMenuTriggerStyle(),
+							"w-full flex justify-start ",
+							{
+								"font-semibold bg-primary": pathname === "/settings",
+							},
+						)}
+					>
+						<span className="">{t("rssFeed")}</span>
+					</Link>
+				</div>
 
-        <SheetFooter className="mt-6 !justify-start flex-row flex-wrap gap-2">
-          <ThemeToggle />
-          <LocaleToggle />
-          <LogoutForm />
-        </SheetFooter>
-      </SheetContent>
-    </Sheet>
-  );
+				<SheetFooter className="mt-6 !justify-start flex-row flex-wrap gap-2">
+					<ThemeToggle />
+					<LocaleToggle />
+					<LogoutForm />
+				</SheetFooter>
+			</SheetContent>
+		</Sheet>
+	);
 }
