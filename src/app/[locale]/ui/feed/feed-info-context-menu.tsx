be43cf4b202c571cef599a129381db87cf38d21b
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2025-01-05 00:33:34 -0500

    feat: pouvoir copier le l'URL d'un feed

--- src/app/[locale]/ui/feed/feed-info-context-menu.tsx
+++ src/app/[locale]/ui/feed/feed-info-context-menu.tsx
@@ -16,7 +16,7 @@ import { Trash } from "lucide-react";
 import { useTranslations } from "next-intl";
 import { useQueryStates } from "nuqs";
 import { useTransition } from "react";
-import { TbSettings } from "react-icons/tb";
+import { TbClipboard, TbSettings } from "react-icons/tb";
 import { toast } from "sonner";
 export function FeedInfoContextMenu({
 	feeds,
@@ -25,6 +25,7 @@ export function FeedInfoContextMenu({
 }): React.JSX.Element {
 	const [{ selectedFeed }] = useQueryStates(searchParamsParsers);
 	const t = useTranslations("rssFeed.info");
+	const feed = feeds.find((feed) => feed.id.toString() === selectedFeed);
 
 	return (
 		<DropdownMenu>
@@ -36,8 +37,7 @@ export function FeedInfoContextMenu({
 				>
 					<TbSettings />
 					{selectedFeed === SELECTED_FEED_DEFAULT && t("allFeeds")}
-					{selectedFeed !== SELECTED_FEED_DEFAULT &&
-						feeds.find((feed) => feed.id.toString() === selectedFeed)?.title}
+					{selectedFeed !== SELECTED_FEED_DEFAULT && feed?.title}
 				</button>
 			</DropdownMenuTrigger>
 			<DropdownMenuContent className="w-56">
@@ -45,6 +45,11 @@ export function FeedInfoContextMenu({
 					<DropdownMenuItem disabled={selectedFeed === SELECTED_FEED_DEFAULT}>
 						<UnfollowFeed />
 					</DropdownMenuItem>
+					{selectedFeed !== SELECTED_FEED_DEFAULT && feed?.url && (
+						<DropdownMenuItem>
+							<CopyFeedURL url={feed.url} />
+						</DropdownMenuItem>
+					)}
 				</DropdownMenuGroup>
 			</DropdownMenuContent>
 		</DropdownMenu>
@@ -88,6 +93,30 @@ function UnfollowFeed(): React.JSX.Element {
 			>
 				<Trash />
 				{t("unfollow")}
+			</Button>
+		</>
+	);
+}
+
+function CopyFeedURL({ url }: { url: string }): React.JSX.Element {
+	const t = useTranslations("rssFeed");
+
+	const handleCopy = async (): Promise<void> => {
+		try {
+			await navigator.clipboard.writeText(url);
+			toast.success(t("feedURLCopied"), {
+				duration: 2000,
+			});
+		} catch (_err) {
+			toast.error(t("errors.cannotCopuFeedURL"));
+		}
+	};
+
+	return (
+		<>
+			<Button onClick={handleCopy} variant={"ghost"} size={"sm"}>
+				<TbClipboard />
+				{t("copyFeedURL")}
 			</Button>
 		</>
 	);
