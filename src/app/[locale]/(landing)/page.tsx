eae4a93344bbc5e45d0a51c3eb22e21705f7ca41
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2025-01-19 00:39:37 -0500

    feat: refonte landing page

--- src/app/[locale]/(landing)/page.tsx
+++ src/app/[locale]/(landing)/page.tsx
@@ -1,4 +1,5 @@
 import { SPACING } from "@/app/[locale]/ui/spacing";
+import BlurFade from "@/components/ui/blur-fade";
 import { Button } from "@/components/ui/button";
 import {
 	Card,
@@ -14,7 +15,6 @@ import {
 	navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu";
 import { ShimmerButton } from "@/components/ui/shimmer-button";
-import { TextAnimate } from "@/components/ui/text-animate";
 import { Link } from "@/i18n/routing";
 import { cn } from "@/lib/utils";
 import { useTranslations } from "next-intl";
@@ -22,6 +22,23 @@ import { TbArchive, TbMail, TbRss } from "react-icons/tb";
 
 export default function Home(): React.JSX.Element {
 	const t = useTranslations("metadata");
+	const elements: {
+		el: () => React.JSX.Element;
+		index: number;
+	}[] = [
+		{
+			el: HeroSection,
+			index: 0,
+		},
+		{
+			el: FeaturesSection,
+			index: 1,
+		},
+		{
+			el: CtaSection,
+			index: 2,
+		},
+	];
 
 	return (
 		<>
@@ -50,94 +67,11 @@ export default function Home(): React.JSX.Element {
 					SPACING.LG,
 				)}
 			>
-				{/* hero */}
-				<section className={cn("text-center", SPACING.MD)}>
-					<TextAnimate
-						className="text-4xl md:text-6xl font-extrabold leading-none tracking-tight"
-						animation="blurInUp"
-						by="line"
-						as={"h1"}
-						once={true}
-					>
-						{t("headline")}
-					</TextAnimate>
-
-					<TextAnimate
-						animation="fadeIn"
-						by="line"
-						className="text-xl"
-						as={"p"}
-						once={true}
-					>
-						{t("description")}
-					</TextAnimate>
-
-					<div className="flex justify-center">
-						<Link href="/login">
-							<ShimmerButton className="shadow-2xl h-12 px-12">
-								{t("ctaButton")}
-							</ShimmerButton>
-						</Link>
-					</div>
-				</section>
-
-				{/* features */}
-				<section
-					className={cn(
-						"grid grid-cols-1 md:grid-cols-3 grid-rows-1 md:grid-rows-2 gap-6 break-words",
-					)}
-				>
-					{t.raw("features")?.map(
-						(feature: {
-							key: string;
-							title: string;
-							description: string;
-						}) => (
-							<Card
-								className={cn(
-									"hover:shadow-md transition-all duration-200 gri grid-rows-subgri col-span1 gridcols-subgrid",
-									{
-										"md:row-start-1 md:col-span-2": feature.key === "0",
-										"md:row-start-1 md:row-span-2": feature.key === "1",
-										"md:row-start-2 md:col-span-2": feature.key === "2",
-									},
-								)}
-								key={feature.key}
-							>
-								<CardHeader>
-									<CardTitle
-										className={cn(
-											"font-extrabold text-xl md:text-2xl",
-											SPACING.SM,
-										)}
-									>
-										<div>
-											{feature.key === "0" && <TbArchive />}
-											{feature.key === "1" && <TbRss />}
-											{feature.key === "2" && <TbMail />}
-										</div>
-										<div>{feature.title}</div>
-									</CardTitle>
-									<CardDescription>{feature.description}</CardDescription>
-								</CardHeader>
-							</Card>
-						),
-					)}
-				</section>
-
-				{/* cta  */}
-				<section className={cn("text-center", SPACING.MD)}>
-					<h2 className="text-3xl md:text-4xl font-bold leading-none tracking-tight">
-						{t("lastCtaHeadline")}
-					</h2>
-
-					<p className="text-lg">{t("lastCtaDescription")}</p>
-					<div>
-						<Link href="/login">
-							<Button size={"lg"}>{t("ctaButton")}</Button>
-						</Link>
-					</div>
-				</section>
+				{elements.map((el) => (
+					<BlurFade key={el.index} delay={0.25 + el.index * 0.05}>
+						{el.el()}
+					</BlurFade>
+				))}
 			</main>
 
 			<footer className="bg-secondary p-10">
@@ -164,3 +98,94 @@ export default function Home(): React.JSX.Element {
 		</>
 	);
 }
+
+const HeroSection = (): React.JSX.Element => {
+	const t = useTranslations("metadata");
+	return (
+		<section className={cn("text-center", SPACING.MD)}>
+			<h1 className="text-4xl md:text-6xl font-extrabold leading-none tracking-tight">
+				{t("headline")}
+			</h1>
+
+			<p className="text-xl">{t("description")}</p>
+
+			<div className="flex justify-center">
+				<Link href="/login">
+					<ShimmerButton className="shadow-2xl h-12 px-12">
+						{t("ctaButton")}
+					</ShimmerButton>
+				</Link>
+			</div>
+		</section>
+	);
+};
+
+const FeaturesSection = (): React.JSX.Element => {
+	const t = useTranslations("metadata");
+
+	return (
+		<section
+			className={cn(
+				"grid grid-cols-1 md:grid-cols-3 grid-rows-1 md:grid-rows-2 gap-6 break-words",
+			)}
+		>
+			{t.raw("features")?.map(
+				(feature: {
+					key: string;
+					title: string;
+					description: string;
+				}) => (
+					<Card
+						className={cn(
+							"hover:shadow-md transition-all duration-200 gri grid-rows-subgri col-span1 gridcols-subgrid",
+							{
+								"md:row-start-1 md:col-span-2": feature.key === "0",
+								"md:row-start-1 md:row-span-2": feature.key === "1",
+								"md:row-start-2 md:col-span-2": feature.key === "2",
+							},
+						)}
+						key={feature.key}
+					>
+						<CardHeader>
+							<CardTitle
+								className={cn("font-extrabold text-xl md:text-2xl", SPACING.SM)}
+							>
+								<div>
+									{feature.key === "0" && (
+										<TbArchive className="hover:text-primary" />
+									)}
+									{feature.key === "1" && (
+										<TbRss className="hover:text-primary" />
+									)}
+									{feature.key === "2" && (
+										<TbMail className="hover:text-primary transition-all duration-200" />
+									)}
+								</div>
+								<div>{feature.title}</div>
+							</CardTitle>
+							<CardDescription>{feature.description}</CardDescription>
+						</CardHeader>
+					</Card>
+				),
+			)}
+		</section>
+	);
+};
+
+const CtaSection = (): React.JSX.Element => {
+	const t = useTranslations("metadata");
+	return (
+		<section className={cn("text-center", SPACING.MD)}>
+			<h2 className="text-3xl md:text-4xl font-bold leading-none tracking-tight">
+				{t("lastCtaHeadline")}
+			</h2>
+
+			<p className="text-lg">{t("lastCtaDescription")}</p>
+			<div>
+				<Link href="/login">
+					<Button size={"lg"}>{t("ctaButton")}</Button>
+				</Link>
+			</div>
+		</section>
+	);
+};
