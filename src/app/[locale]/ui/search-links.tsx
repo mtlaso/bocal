890fc7530c7ce3f39432006f1d5e56771552a3dd
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2025-01-21 03:02:52 -0500

    feat: state management de qui est recherche sur mobile

--- src/app/[locale]/ui/search-links.tsx
+++ src/app/[locale]/ui/search-links.tsx
@@ -7,7 +7,7 @@ import { Label } from "@/components/ui/label";
 import { useTranslations } from "next-intl";
 import { useQueryStates } from "nuqs";
 import { useRef, useState } from "react";
-import { TbSearch } from "react-icons/tb";
+import { TbSearch, TbX } from "react-icons/tb";
 
 export function SearchLinksDesktop(): React.JSX.Element {
 	const [{ searchedLink }, setSearchedLink] =
@@ -43,7 +43,8 @@ export function SearchLinksDesktop(): React.JSX.Element {
 	return (
 		<div className="hidden md:flex md:gap-2">
 			<Button onClick={handleOpen} variant="outline" size="icon">
-				<TbSearch />
+				{!open && <TbSearch />}
+				{open && <TbX />}
 			</Button>
 			<div
 				className={`transition-all duration-200 ease-out
@@ -75,6 +76,8 @@ export function SearchLinksDesktop(): React.JSX.Element {
 }
 
 export function SearchLinksMobile(): React.JSX.Element {
+	const [{ searchedLink }, setSearchedLink] =
+		useQueryStates(searchParamsParsers);
 	const t = useTranslations("navbar.search-links");
 	return (
 		<div className="md:hidden">
@@ -88,6 +91,10 @@ export function SearchLinksMobile(): React.JSX.Element {
 					autoFocus
 					id="search-links"
 					placeholder={t("search")}
+					defaultValue={searchedLink}
+					onChange={(e): Promise<URLSearchParams> =>
+						setSearchedLink({ searchedLink: e.currentTarget.value })
+					}
 				/>
 
 				<TbSearch className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
