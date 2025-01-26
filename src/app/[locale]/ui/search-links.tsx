ba04bb0587783f42ceecf93d072628f385232c4c
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2025-01-25 20:27:22 -0500

    feat: ajout bouton 'X' sur les champs de recherche de liens

--- src/app/[locale]/ui/search-links.tsx
+++ src/app/[locale]/ui/search-links.tsx
@@ -50,25 +50,34 @@ export function SearchLinksDesktop(): React.JSX.Element {
 				className={`transition-all duration-200 ease-out
 				 ${open ? "w-64 opacity-100" : "w-0 opacity-0"}`}
 			>
-				<Label htmlFor="search-links" className="sr-only">
+				<Label htmlFor="search-links-desktop" className="sr-only">
 					{t("search")}
 				</Label>
 
 				<div className="relative">
 					<Input
+						id="search-links-desktop"
 						ref={inputRef}
 						className="rounded-md border py-2 pl-10 outline-2 placeholder:text-gray-500"
 						autoFocus={open}
-						id="search-links"
 						placeholder={t("search")}
 						onKeyDown={handleCloseWithEscape}
-						defaultValue={searchedLink}
+						value={searchedLink}
 						onChange={(e): Promise<URLSearchParams> =>
 							setSearchedLink({ searchedLink: e.currentTarget.value })
 						}
 					/>
 
 					<TbSearch className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+					<button
+						type="button"
+						onClick={(_e): Promise<URLSearchParams> =>
+							setSearchedLink({ searchedLink: "" })
+						}
+						className="absolute right-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500"
+					>
+						<TbX />
+					</button>
 				</div>
 			</div>
 		</div>
@@ -81,22 +90,32 @@ export function SearchLinksMobile(): React.JSX.Element {
 	const t = useTranslations("navbar.search-links");
 	return (
 		<div className="md:hidden">
-			<Label htmlFor="search-links" className="sr-only">
+			<Label htmlFor="search-links-mobile" className="sr-only">
 				{t("search")}
 			</Label>
 
 			<div className="relative">
 				<Input
 					className="rounded-md border py-2 pl-10 outline-2 placeholder:text-gray-500"
-					id="search-links"
+					id="search-links-mobile"
 					placeholder={t("search")}
-					defaultValue={searchedLink}
+					value={searchedLink}
 					onChange={(e): Promise<URLSearchParams> =>
 						setSearchedLink({ searchedLink: e.currentTarget.value })
 					}
 				/>
 
 				<TbSearch className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
+
+				<button
+					type="button"
+					onClick={(_e): Promise<URLSearchParams> =>
+						setSearchedLink({ searchedLink: "" })
+					}
+					className="absolute right-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500"
+				>
+					<TbX />
+				</button>
 			</div>
 		</div>
 	);
