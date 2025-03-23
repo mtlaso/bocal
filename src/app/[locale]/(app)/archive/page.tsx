701915ae171c479ff0668f27e0cda2a6e3d91bcb
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2025-03-22 21:01:20 -0400

    style: couleur rouge

--- src/app/[locale]/(app)/archive/page.tsx
+++ src/app/[locale]/(app)/archive/page.tsx
@@ -1,6 +1,6 @@
 import {
-	SearchLinksDesktop,
-	SearchLinksMobile,
+  SearchLinksDesktop,
+  SearchLinksMobile,
 } from "@/app/[locale]/ui/search-links";
 import { LinksSkeleton } from "@/app/[locale]/ui/skeletons";
 import { SortLinks } from "@/app/[locale]/ui/sort-links";
@@ -13,58 +13,58 @@ import { getLinks } from "../../lib/data";
 import { Links } from "../../ui/links";
 
 export async function generateMetadata({
-	params,
+  params,
 }: {
-	params: Promise<{ locale: string }>;
+  params: Promise<{ locale: string }>;
 }): Promise<Metadata> {
-	const { locale } = await params;
-	const t = await getTranslations({ locale, namespace: "metadata.archive" });
+  const { locale } = await params;
+  const t = await getTranslations({ locale, namespace: "metadata.archive" });
 
-	return {
-		title: t("title"),
-		description: t("description"),
-	} satisfies Metadata;
+  return {
+    title: t("title"),
+    description: t("description"),
+  } satisfies Metadata;
 }
 
 export default async function Page(): Promise<React.JSX.Element> {
-	const t = await getTranslations("archive");
+  const t = await getTranslations("archive");
 
-	return (
-		<>
-			<section className={SPACING.SM}>
-				<div className="flex justify-between">
-					<div className="flex gap-2 w-full">
-						<h1 className="font-semibold tracking-tight text-3xl">
-							{t("archive")}
-						</h1>
-						<Suspense>
-							<SearchLinksDesktop />
-						</Suspense>
-					</div>
+  return (
+    <>
+      <section className={SPACING.SM}>
+        <div className="flex justify-between">
+          <div className="flex gap-2 w-full">
+            <h1 className="font-semibold tracking-tight text-3xl">
+              {t("archive")}
+            </h1>
+            <Suspense>
+              <SearchLinksDesktop />
+            </Suspense>
+          </div>
 
-					<div>
-						<Suspense>
-							<SortLinks />
-						</Suspense>
-					</div>
-				</div>
+          <div>
+            <Suspense>
+              <SortLinks />
+            </Suspense>
+          </div>
+        </div>
 
-				<Suspense>
-					<SearchLinksMobile />
-				</Suspense>
-			</section>
+        <Suspense>
+          <SearchLinksMobile />
+        </Suspense>
+      </section>
 
-			<Separator className="my-4" />
+      <Separator className="my-4" />
 
-			<Suspense fallback={<LinksSkeleton />}>
-				<LinksWrapper />
-			</Suspense>
-		</>
-	);
+      <Suspense fallback={<LinksSkeleton />}>
+        <LinksWrapper />
+      </Suspense>
+    </>
+  );
 }
 
 async function LinksWrapper(): Promise<React.JSX.Element> {
-	const links = await getLinks({ archivedLinksOnly: true });
+  const links = await getLinks({ archivedLinksOnly: true });
 
-	return <Links links={links} view={"grid"} />;
+  return <Links links={links} />;
 }
