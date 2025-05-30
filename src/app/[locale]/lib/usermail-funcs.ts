44bf6c5d594d4cacf9f4da0ecbbe08c9cf379f31
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2025-05-30 05:14:03 -0400

    fix: changer url de récéption des newsletters

--- src/app/[locale]/lib/usermail-funcs.ts
+++ src/app/[locale]/lib/usermail-funcs.ts
@@ -6,12 +6,12 @@ const NEWSLETTER_URL_PREFIX = "https://bocal.fyi/userfeeds/";
 /**
  * USERMAIL_DOMAIN is the domain part of the address a user needs to use to receive newsletters.
  */
-const USERMAIL_DOMAIN = "usermail.bocal.fyi";
+const USERMAIL_DOMAIN = "bocalusermail.fyi";
 
 /**
- * format returns the email a user can use to receive newsletter at.
+ * format returns the email a user can use to receive newsletters at.
  * A 'usermail' email address follows this structure:
- * <feed-external-id>@usermail.bocal.fyi.
+ * <feed-external-id>@bocalusermail.fyi.
  * @param feedEid External id (eid) of a feed.
  */
 function format(feedEid: string): string {
