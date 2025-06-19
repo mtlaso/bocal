75d3866eb2deed79c8f317113a45debea7d6c940
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2025-06-18 21:20:03 -0400

    fix: corriger erreurs de migrations en ajoutant IF EXISTS

--- drizzle/0008_numerous_karnak.sql
+++ drizzle/0008_numerous_karnak.sql
@@ -1,2 +1,2 @@
-ALTER TABLE "feeds" DROP CONSTRAINT "feeds_eid_unique";--> statement-breakpoint
-DROP INDEX "url";
+ALTER TABLE "feeds" DROP CONSTRAINT IF EXISTS "feeds_eid_unique";--> statement-breakpoint
+DROP INDEX IF EXISTS "url";
