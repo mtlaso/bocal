004323025dc5a685527459e05959db64864f1cc5
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2025-06-22 03:27:19 -0400

    fix: améliorer lecteur feed content

---
+++ src/app/[locale]/(userfeeds)/userfeeds/[slug]/content/[feedContentEID]/route.tsx
@@ -0,0 +1,46 @@
+import { notFound } from "next/navigation";
+import { type NextRequest, NextResponse } from "next/server";
+import { feedService } from "@/app/[locale]/lib/feed-service";
+export async function GET(
+	_request: NextRequest,
+	{
+		params,
+	}: {
+		params: Promise<{ feedContentEID: string }>;
+	},
+) {
+	const { feedContentEID } = await params;
+	if (!feedContentEID) {
+		notFound();
+	}
+
+	const { content, error } = await feedService.getFeedContent(feedContentEID);
+	if (error) {
+		switch (error) {
+			case "invalid-eid":
+				return new NextResponse("invalid feed content ID", {
+					status: 400,
+				});
+			case "not-found":
+				return new NextResponse("feed content not found", {
+					status: 404,
+				});
+			default:
+				return new NextResponse("internal server error", {
+					status: 500,
+				});
+		}
+	}
+
+	if (!content) {
+		return new NextResponse("Error fetching feed content", {
+			status: 400,
+		});
+	}
+
+	return new Response(content, {
+		headers: {
+			"Content-Type": "text/html; charset=utf-8;",
+		},
+	});
+}
