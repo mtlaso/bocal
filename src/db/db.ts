91c046a8e26ccce9ceb51d24c1f34a8d79355ae6
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2025-06-30 03:20:37 -0400

    fix: db

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,10 +1,28 @@
+import { neon } from "@neondatabase/serverless";
+import { drizzle as drizzleNeon } from "drizzle-orm/neon-http";
 import { drizzle } from "drizzle-orm/node-postgres";
 import { Pool } from "pg";
 
 import * as schema from "./schema";
 
-const pool = new Pool({
-	connectionString: process.env.DATABASE_URL,
-});
+// biome-ignore lint/suspicious/noImplicitAnyLet: db.
+let _db;
 
-export const db = drizzle({ client: pool, schema, logger: false });
+if (!process.env.VERCEL_ENV) {
+	_db = drizzle({
+		client: new Pool({
+			connectionString: process.env.DATABASE_URL,
+		}),
+		schema,
+		logger: false,
+	});
+} else {
+	_db = drizzleNeon({
+		// biome-ignore lint/style/noNonNullAssertion: db.
+		client: neon(process.env.DATABASE_URL!),
+		schema,
+		logger: false,
+	});
+}
+
+export const db = _db;
