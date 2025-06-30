7e7d3688de2b4ab5d3e9823596d99b8300548930
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2025-06-30 03:08:24 -0400

    refactor: simplifier ficher bd

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,24 +1,10 @@
-import { Pool } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-serverless";
-
-import { drizzle as drizzlePg } from "drizzle-orm/node-postgres";
-import { Pool as PoolPg } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
+import { Pool } from "pg";
 
 import * as schema from "./schema";
 
-// biome-ignore lint/suspicious/noImplicitAnyLet: locale exception.
-let pool;
-// biome-ignore lint/suspicious/noImplicitAnyLet: locale exception.
-let _db;
+const pool = new Pool({
+	connectionString: process.env.DATABASE_URL,
+});
 
-if (!process.env.VERCEL_ENV) {
-	pool = new PoolPg({
-		connectionString: process.env.DATABASE_URL,
-	});
-	_db = drizzlePg({ client: pool, schema, logger: false });
-} else {
-	pool = new Pool({ connectionString: process.env.DATABASE_URL });
-	_db = drizzle({ client: pool, schema, logger: false });
-}
-
-export const db = _db;
+export const db = drizzle({ client: pool, schema, logger: false });
