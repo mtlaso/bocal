df5e672e90192a73f27057bde6ae9bac6f8d8b39
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2025-07-08 21:39:35 -0400

    fix: retourner null pour env prod

--- src/components/ui/base-layout.tsx
+++ src/components/ui/base-layout.tsx
@@ -6,49 +6,51 @@ import ThemeProvider from "@/components/theme-provider";
 import { Toaster } from "@/components/ui/sonner";
 
 type Props = {
-	children: React.ReactNode;
-	locale: string;
+  children: React.ReactNode;
+  locale: string;
 };
 
 export default async function BaseLayout({
-	children,
-	locale,
+  children,
+  locale,
 }: Props): Promise<React.JSX.Element> {
-	return (
-		<html lang={locale} suppressHydrationWarning>
-			<body className={`${inter.className} antialiased`}>
-				<NextIntlClientProvider>
-					<ThemeProvider
-						attribute="class"
-						defaultTheme="system"
-						enableSystem
-						disableTransitionOnChange={false}
-					>
-						<DevIndicator />
-						<NuqsAdapter>{children}</NuqsAdapter>
-						<Toaster />
-					</ThemeProvider>
-				</NextIntlClientProvider>
-			</body>
-			<Script defer src="https://assets.onedollarstats.com/stonks.js" />
-		</html>
-	);
+  return (
+    <html lang={locale} suppressHydrationWarning>
+      <body className={`${inter.className} antialiased`}>
+        <NextIntlClientProvider>
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange={false}
+          >
+            <DevIndicator />
+            <NuqsAdapter>{children}</NuqsAdapter>
+            <Toaster />
+          </ThemeProvider>
+        </NextIntlClientProvider>
+      </body>
+      <Script defer src="https://assets.onedollarstats.com/stonks.js" />
+    </html>
+  );
 }
 
 function DevIndicator() {
-	const env = process.env.NEXT_PUBLIC_VERCEL_ENV;
-	if (env === "development") {
-		return (
-			<div className="fixed bottom-5 right-[50%] translate-x-1/2 z-50 bg-red-300 dark:bg-red-900 px-8 py-2 rounded-full">
-				{env}
-			</div>
-		);
-	}
-	if (env === "preview") {
-		return (
-			<div className="fixed bottom-5 right-[50%] translate-x-1/2 z-50 bg-green-300 dark:bg-green-900 px-8 py-2 rounded-full">
-				{env}
-			</div>
-		);
-	}
+  const env = process.env.NEXT_PUBLIC_VERCEL_ENV;
+  if (env === "development") {
+    return (
+      <div className="fixed bottom-5 right-[50%] translate-x-1/2 z-50 bg-red-300 dark:bg-red-900 px-8 py-2 rounded-full">
+        {env}
+      </div>
+    );
+  }
+  if (env === "preview") {
+    return (
+      <div className="fixed bottom-5 right-[50%] translate-x-1/2 z-50 bg-green-300 dark:bg-green-900 px-8 py-2 rounded-full">
+        {env}
+      </div>
+    );
+  }
+
+  return null;
 }
