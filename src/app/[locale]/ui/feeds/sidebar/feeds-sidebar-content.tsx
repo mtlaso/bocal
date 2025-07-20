8f13868ba7374a3f976c412b71f8aef19358e84d
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2025-07-19 22:48:01 -0400

    feat: afficher le nombre de flux

--- src/app/[locale]/ui/feeds/sidebar/feeds-sidebar-content.tsx
+++ src/app/[locale]/ui/feeds/sidebar/feeds-sidebar-content.tsx
@@ -1,17 +1,25 @@
+import { getTranslations } from "next-intl/server";
 import { dal } from "@/app/[locale]/lib/dal";
 import { FeedsSidebarMenuItem } from "@/app/[locale]/ui/feeds/sidebar/feeds-sidebar-menu-item";
 import { FeedsSidebarMenuItemAll } from "@/app/[locale]/ui/feeds/sidebar/feeds-sidebar-menu-item-all";
-import { SidebarMenu } from "@/components/ui/sidebar";
+import { SidebarMenu, SidebarMenuItem } from "@/components/ui/sidebar";
 
 export async function FeedsSidebarContent(): Promise<React.JSX.Element> {
-	const [timeline, userFeedsWithContentsCount] = await Promise.all([
+	const [timeline, userFeedsWithContentsCount, t] = await Promise.all([
 		dal.getUserFeedsTimeline(),
 		dal.getUserFeedsWithContentsCount(),
+		getTranslations("rssFeed.info"),
 	]);
 
 	return (
 		<SidebarMenu>
 			<FeedsSidebarMenuItemAll totalFeedsContents={timeline[0].length} />
+
+			<SidebarMenuItem className="mt-4">
+				<span>
+					{t("textFeedsCount", { count: userFeedsWithContentsCount.length })}
+				</span>
+			</SidebarMenuItem>
 
 			{userFeedsWithContentsCount.map((feed) => (
 				<FeedsSidebarMenuItem key={feed.id} feed={feed} />
