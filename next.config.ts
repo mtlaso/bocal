c030bc430dc360bf2a4a0b3d73a50a3ca16ade05
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2025-07-23 22:40:24 -0400

    feat: utiliser nouvelle fonctionnalites de nextjs

--- next.config.ts
+++ next.config.ts
@@ -1,27 +1,40 @@
-import createMDX from '@next/mdx'
+import createMDX from "@next/mdx";
 import createNextIntlPlugin from "next-intl/plugin";
 import { type NextConfig } from "next";
 
-const withNextIntl = createNextIntlPlugin()
-
 const nextConfig: NextConfig = {
-  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   images: {
     remotePatterns: [
       {
         protocol: undefined,
-        hostname: "**"
+        hostname: "**",
       },
     ],
   },
   experimental: {
-    ppr: "incremental"
-  }
+    // Forward browser logs to the terminal for easier debugging
+    browserDebugInfoInTerminal: true,
 
+    // Enable new caching and pre-rendering behavior
+    // dynamicIO: true, // will be renamed to cacheComponents in Next.js 16
+    ppr: "incremental",
+
+    // Activate new client-side router improvements
+    clientSegmentCache: true,
+
+    // Explore route composition and segment overrides via DevTools
+    devtoolSegmentExplorer: true,
+
+    // Enable support for `global-not-found`, which allows you to more easily define a global 404 page.
+    globalNotFound: true,
+
+    // Enable persistent caching for the turbopack dev server and build.
+    turbopackPersistentCaching: true,
+  },
 } satisfies NextConfig;
 
-
-const withMDX = createMDX({
-})
+const withNextIntl = createNextIntlPlugin();
+const withMDX = createMDX({});
 
 export default withNextIntl(withMDX(nextConfig));
