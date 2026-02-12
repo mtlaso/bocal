7417424071e912c034124b6caaa7d115094500ea
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2026-02-11 23:10:00 -0500

    shit

--- src/auth.ts
+++ src/auth.ts
@@ -73,9 +73,7 @@ export const auth = betterAuth({
 			// https://www.better-auth.com/docs/plugins/oauth-proxy#add-redirect-url-to-your-oauth-provider
 			// > For the proxy server to work properly, you’ll need to pass the redirect URL of your main production app
 			// ...registered with the OAuth provider in your social provider config
-			redirectURI: process.env.VERCEL_ENV
-				? "https://www.bocal.fyi/api/auth/callback/github"
-				: "http://localhost:3000/api/auth/callback/github",
+			// redirectURI: "https://www.bocal.fyi/api/auth/callback/github",
 		},
 		google: {
 			// https://www.better-auth.com/docs/authentication/google#always-get-refresh-tokenaccessType: "offline",
@@ -86,9 +84,7 @@ export const auth = betterAuth({
 			// https://www.better-auth.com/docs/plugins/oauth-proxy#add-redirect-url-to-your-oauth-provider
 			// > For the proxy server to work properly, you’ll need to pass the redirect URL of your main production app
 			// ...registered with the OAuth provider in your social provider config
-			redirectURI: process.env.VERCEL_ENV
-				? "https://www.bocal.fyi/api/auth/callback/google"
-				: "http://localhost:3000/api/auth/callback/google",
+			// redirectURI: "https://www.bocal.fyi/api/auth/callback/google",
 		},
 	},
 	plugins: [
