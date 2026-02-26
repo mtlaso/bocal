a5db370f91460d93342322c7bae9414f905ce025
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2026-02-25 20:14:58 -0500

    refactor: retier code inutile

--- src/lib/parsing.client.ts
+++ src/lib/parsing.client.ts
@@ -1,5 +1,4 @@
 "use client";
-import DOMPurify from "isomorphic-dompurify";
 
 /**
  * readableUrl retourne une URL lisible. Retirer le 'www.' ou le 'http(s)://'.
@@ -18,68 +17,8 @@ const readableUrl = (url: string): string => {
 };
 
 /**
- * sanitizeHTML retire les balises HTML non autorisées.
- */
-function sanitizeHTML(html: string): string {
-	return DOMPurify.sanitize(html, {
-		ALLOWED_TAGS: [
-			"h1",
-			"h2",
-			"h3",
-			"h4",
-			"h5",
-			"h6",
-			"div",
-			"span",
-			"section",
-			"article",
-			"ul",
-			"ol",
-			"li",
-			"dl",
-			"dt",
-			"dd",
-			"table",
-			"thead",
-			"tbody",
-			"tfoot",
-			"tr",
-			"th",
-			"td",
-			"p",
-			"a",
-			"b",
-			"i",
-			"em",
-			"strong",
-			"br",
-			"img",
-			"figure",
-			"figcaption",
-			"video",
-			"audio",
-		],
-		ALLOWED_ATTR: [
-			"style",
-			"href",
-			"src",
-			"alt",
-			"width",
-			"height",
-			"colspan",
-			"rowspan",
-			"scope", // for tables
-			"type",
-			"start",
-			"reversed", // for lists
-		],
-	});
-}
-
-/**
  * parser contient les fonctions de parse.
  */
 export const parsing = {
 	readableUrl,
-	sanitizeHTML,
 };
