4a6ce61939a7b43b854ff6512182999e5e45d6f7
dnncrye.dev <29934021+mtlaso@users.noreply.github.com>
2026-02-25 20:34:36 -0500

    retirer parsing dans links pour savoir si ca fonctionne

--- src/app/[locale]/ui/auth/login-form.tsx
+++ src/app/[locale]/ui/auth/login-form.tsx
@@ -3,41 +3,42 @@
 import { useTranslations } from "next-intl";
 import { useState } from "react";
 import { FaGithub, FaGoogle } from "react-icons/fa";
-import { toast } from "sonner";
 import { SPACING } from "@/app/[locale]/ui/spacing";
 import { Button } from "@/components/ui/button";
 import { authenticate } from "@/lib/actions";
 
 export function LoginForm(): React.JSX.Element {
 	const t = useTranslations("login");
-	const [isDisabled, setIsDisabled] = useState(false);
-	const [redirectingMsg, setRedirectingMsg] = useState("");
+	const [isDisabled, _setIsDisabled] = useState(false);
+	const [redirectingMsg, _setRedirectingMsg] = useState("");
 
 	const handleProviderSignIn = async (
 		e: React.MouseEvent,
 		provider: string,
 	): Promise<void> => {
-		try {
-			setIsDisabled(true);
-			setRedirectingMsg(t("redirecting"));
-			e.preventDefault();
-			await authenticate(provider);
-		} catch (err) {
-			// Ignorer erreurs NEXT_REDIRECT par auth.js pendant la redirection.
-			const isNextRedirect =
-				err instanceof Error &&
-				(((err as Error & { digest?: string }).digest?.startsWith(
-					"NEXT_REDIRECT;",
-				) ??
-					false) ||
-					err.message === "NEXT_REDIRECT");
-			if (!isNextRedirect) {
-				toast.error(t("errors.unexpected"));
-				setRedirectingMsg("");
-			}
-		} finally {
-			setIsDisabled(false);
-		}
+		e.preventDefault();
+		await authenticate(provider);
+		// try {
+		// 	setIsDisabled(true);
+		// 	setRedirectingMsg(t("redirecting"));
+		// 	e.preventDefault();
+		// 	await authenticate(provider);
+		// } catch (err) {
+		// 	// Ignorer erreurs NEXT_REDIRECT par auth.js pendant la redirection.
+		// 	const isNextRedirect =
+		// 		err instanceof Error &&
+		// 		(((err as Error & { digest?: string }).digest?.startsWith(
+		// 			"NEXT_REDIRECT;",
+		// 		) ??
+		// 			false) ||
+		// 			err.message === "NEXT_REDIRECT");
+		// 	if (!isNextRedirect) {
+		// 		toast.error(t("errors.unexpected"));
+		// 		setRedirectingMsg("");
+		// 	}
+		// } finally {
+		// 	setIsDisabled(false);
+		// }
 	};
 
 	return (
